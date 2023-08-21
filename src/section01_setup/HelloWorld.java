1fa204eca12a81a2e7a90d62d848bd3a14cadd76
dan <mrdocula@gmail.com>
2023-08-21 20:26:20 +0300

    first commit1

---
+++ src/section01_setup/HelloWorld.java
@@ -0,0 +1,7 @@
+package section01_setup;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/section01_setup/ILoveJava.java
@@ -0,0 +1,8 @@
+package section01_setup;
+
+public class ILoveJava {
+    public static void main(String[] args) {
+        System.out.print("Hello, ");
+        System.out.println("I love Java!");
+    }
+}
